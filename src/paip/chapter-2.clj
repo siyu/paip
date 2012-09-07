3ecfec12fa03484faee8dc597f1c37dd911d388b
Si Yu <siyu798@gmail.com>
2012-09-07 00:36:05 -0400

    refactored chapter 2

--- src/paip/chapter-2.clj
+++ src/paip/chapter-2.clj
@@ -1,34 +1,32 @@
 (ns paip.chapter-2)
 
 (def simple-grammar
-  {"sentence" [["noun-phrase" "verb-phrase"]]
-   "noun-phrase" [["Article" "Noun"]]
-   "verb-phrase" [["Verb" "noun-phrase"]]
-   "Article" ["the" "a"]
-   "Noun" ["man" "ball" "woman" "table"]
-   "Verb" ["hit" "took" "saw" "liked"]})
+  {:sentence [[:noun-phrase :verb-phrase]]
+   :noun-phrase [[:Article :Noun]]
+   :verb-phrase [[:Verb :noun-phrase]]
+   :Article ["the" "a"]
+   :Noun ["man" "ball" "woman" "table"]
+   :Verb ["hit" "took" "saw" "liked"]})
 
 (def bigger-grammar
-  {"sentence" [["noun-phrase" "verb-phrase"]]
-   "noun-phrase" [["Article" "Adj*" "Noun" "PP*"] ["Name"] ["Pronoun"]]
-   "verb-phrase" [["Verb" "noun-phrase" "PP*"]]
-   "PP*" [[] ["PP" "PP*"]]
-   "Adj*" [[] ["Adj" "Adj*"]]
-   "PP" [["Prep" "noun-phrase"]]
-   "Prep" ["to" "in" "by" "with" "on"]
-   "Adj" ["big" "little" "blue" "green" "adiabatic"]
-   "Article" ["the" "a"]
-   "Name" ["Pat" "Kim" "Lee" "Terry" "Robin"]
-   "Noun" ["man" "ball" "woman" "table"]
-   "Verb" ["hit" "took" "saw" "liked"]
-   "Pronoun" ["he" "she" "it" "these" "those" "that"]})
+  {:sentence [[:noun-phrase :verb-phrase]]
+   :noun-phrase [[:Article :Adj* :Noun :PP*] [:Name] [:Pronoun]]
+   :verb-phrase [[:Verb :noun-phrase :PP*]]
+   :PP* [[] [:PP :PP*]]
+   :Adj* [[] [:Adj :Adj*]]
+   :PP [[:Prep :noun-phrase]]
+   :Prep ["to" "in" "by" "with" "on"]
+   :Adj ["big" "little" "blue" "green" "adiabatic"]
+   :Article ["the" "a"]
+   :Name ["Pat" "Kim" "Lee" "Terry" "Robin"]
+   :Noun ["man" "ball" "woman" "table"]
+   :Verb ["hit" "took" "saw" "liked"]
+   :Pronoun ["he" "she" "it" "these" "those" "that"]})
 
 (defn make-generate [grammar]
   (fn gen [phrase]
-    (cond (coll? phrase)
-          (mapcat gen phrase)
-          (coll? (grammar phrase))
-          (gen (rand-nth (grammar phrase)))
+    (cond (coll? phrase) (mapcat gen phrase)
+          (coll? (grammar phrase)) (gen (rand-nth (grammar phrase)))
           :else [phrase])))
 
 (def generate-simple (make-generate simple-grammar))
@@ -36,10 +34,8 @@
 
 (defn make-generate-tree [grammar]
   (fn gen [phrase]
-    (cond (coll? phrase)
-          (map gen phrase)
-          (coll? (grammar phrase))
-          (conj [phrase] (gen (rand-nth (grammar phrase))))
+    (cond (coll? phrase) (mapcat gen phrase)
+          (coll? (grammar phrase)) (conj [phrase] (gen (rand-nth (grammar phrase))))
           :else [phrase])))
 
 (def generate-tree-simple (make-generate-tree simple-grammar))
